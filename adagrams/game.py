cfeb9370e7e7a3905583718d110b2d047a2a5410
Shameka <Shamekam.edwards@gmail.com>
2026-09-13 01:35:20 -0500

    Wave 3 score word complete

--- adagrams/game.py
+++ adagrams/game.py
@@ -72,7 +72,45 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    #one parameter: word    
+    #returns integer score for the word based on the scoring rules
+    #sum the score for each letter in word
+    # if length of word is 7-10, add 8 pts to score
+    #keep running total of score and return it at the end
+    #create a dictionary of letter scores
+    total_score = 0
+    letter_scores = {
+        "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
+        "D": 2, "G": 2,
+        "B": 3, "C": 3, "M": 3, "P": 3,
+        "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
+        "K": 5,
+        "J": 8, "X": 8,
+        "Q": 10, "Z": 10
+    }
+    word = word.upper()
+    #go thru each letter in the word and add its score to the total score
+    for letter in word:
+        total_score = total_score + letter_scores[letter]
+    if len(word)>= 7 and len(word) <= 10:
+        total_score = total_score + 8
+
+    return total_score
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def get_highest_word_score(word_list):
     pass
